e315614547dea2c17785cc9f818e59ca99c9f73d
OmerAtayilmaz <omeratayilmaz@gmail.com>
2022-12-18 23:24:12 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld :msg="msg" />
+  <input v-model="msg" />
+  <button @click="getData()">Get Data</button>
+  <ul>
+    <li v-for="item in listItems" :key="item.index">
+      {{ item.title }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -10,6 +17,41 @@ export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  methods: {
+    getMessage() {
+      return this.msg;
+    },
+    async getData() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const finalRes = await res.json();
+      this.listItems = finalRes;
+      console.log(this.getData());
+    },
+  },
+  props: ["value", "disabled", "user"],
+  data() {
+    return {
+      msg: "Hello From Parent Componentasdsad",
+      items: [
+        {
+          message: "First Message",
+          index: 1,
+        },
+        {
+          message: "Second Message",
+          index: 2,
+        },
+      ],
+      listItems: [],
+    };
+  },
+  created() {
+    console.log("runnig");
+    console.log(this.user);
+  },
+  mounted() {
+    console.log("mounted running");
   },
 };
 </script>
